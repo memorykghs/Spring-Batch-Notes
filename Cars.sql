43cf4bd1f1428e9ab50637558b080a82aed13e09
memorykghs <37746042+memorykghs@users.noreply.github.com>
2021-10-13 23:42:53 +0800

    Update Cars.sql

--- Cars.sql
+++ Cars.sql
@@ -1,7 +1,18 @@
+-- Oracle
 CREATE TABLE "Ashley"."CARS" 
-	(
-		"MANUFACTURER" VARCHAR2(10 BYTE) PRIMARY KEY, 
-		"TYPE" VARCHAR2(20 BYTE) PRIMARY KEY, 
-		"MIN_PRICE" NUMBER(19,2), 
-		"PRICE" NUMBER(19,2)
-	);
+(
+	"MANUFACTURER" VARCHAR2(10 BYTE) PRIMARY KEY, 
+	"TYPE" VARCHAR2(20 BYTE) PRIMARY KEY, 
+	"MIN_PRICE" NUMBER(19,2), 
+	"PRICE" NUMBER(19,2)
+);
+
+-- MySQL
+create table Ashley.CARS 
+(
+	MANUFACTURER nvarchar(10) not null, 
+	TYPE nvarchar(20) not null, 
+	MIN_PRICE float(19,2), 
+	PRICE float(19,2),
+    	primary key (MANUFACTURER, TYPE)
+) engine=InnoDB default charset=utf8;
